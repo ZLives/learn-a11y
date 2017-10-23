b1a41844e9635b4f72c1cf70bb03a70b1a809007
ZLives <lasse.litmanen@hotmail.fi>
2017-10-23 03:16:50 +0300

    updated first couple of files

--- js/modal.js
+++ js/modal.js
@@ -10,9 +10,37 @@ cancelButton.addEventListener('click', close);
 // https://github.com/jkup/focusable
 
 function open() {
+	var previouslyFocused = document.activeElement;
+
+	var focusableElements = modal.querySelectorAll('a[href], area[href], input:not([disabled]), select:not([disabled]), textarea:not([disabled]), button:not([disabled]), iframe, object, embed, [tabindex="0"], [contenteditable], audio[controls], video[controls]');
+	focusableElements = Array.prototype.slice.call(focusableElements);
+
+	var firstItem = focusableElements[0];
+	var lastItem = focusableElements[focusableElements.length - 1];
+
+	modal.addEventListener('keydown', trap);
   // Show the modal and overlay
   modal.style.display = 'block';
   modalOverlay.style.display = 'block';
+
+  function trap(event) {
+  	if (event.keycode === 9) {
+  		// Shift is hekd down
+	  	if (event.shiftKey) {
+	  		// Backwards
+	  		if (document.activeElement === firstItem) {
+	  			event.preventDefault();
+	  			lastItem.focus();
+	  		}
+	  	} else {
+	  		// forwards
+	  		if (document.activeElement === lastItem) {
+	  			event.preventDefault();
+	  			firstItem.focus();
+	  		}
+	  	}
+  	}
+  }
 }
 
 function close() {
